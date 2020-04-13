16e09da6a867fc271fdbf67cc3c58549e24dbcd4
Charlyln <charleshenry.lenue@gmail.com>
2020-04-13 23:52:01 +0200

    I install react reveal, and add an effect to the virus button when you click on

--- src/components/VirusButton.jsx
+++ src/components/VirusButton.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import Virus from "./virus.png";
+import Tada from "react-reveal/Tada";
+import "./VirusButton.css";
 
 class VirusButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { counter: 0 };
+  }
+  handleClick() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
   render() {
     return (
       <div>
-        <img
-          src={Virus}
-          alt="logo"
-          style={{ width: "500px", margin: "100px" }}
-        />
+        <Tada spy={this.state.counter}>
+          <img
+            className="logo"
+            onClick={this.handleClick}
+            src={Virus}
+            alt="logo"
+            style={{ width: "400px", margin: "100px" }}
+          />
+        </Tada>
       </div>
     );
   }
