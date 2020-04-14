159756bf590c66ed27e089347590c855279cd9c6
Christellec64 <christelle.cwcs64@gmail.com>
2020-04-14 11:46:55 +0200

    adding elements by defaults in id, logo, team, import axios in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import user from './components/user_infos'
+import user from './components/user_infos';
+import axios from 'axios';
 
 function App() {
   return (
     <div className="App">
-      <user/>
+      <user id={this.state.id} logo={this.state.logo} team={this.state.team}/>
     </div>
   );
 }
--- src/components/user_infos.jsx
+++ src/components/user_infos.jsx
@@ -4,16 +4,16 @@ class user extends React.Component{
     constructor (props){
         super(props);
         this.state = {
-            id : "",
-            logo: "",
-            team: "",
+            id : "Pseudo",
+            logo: "https://via.placeholder.com/50",
+            team: "Wilders",
         }
     }
     render () {
         return(
             <div>
                 <h2>{this.state.id}</h2>
-                <img src={this.state.logo} alt={this.state.team}></img>
+                <img src={this.state.logo} alt={this.state.team}/>
                 <h3>{this.state.team}</h3>
             </div>
         )
