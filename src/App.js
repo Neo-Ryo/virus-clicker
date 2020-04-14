7637a65ed143ced3df26b6e609eb9938a041be46
Marion-H <doudou6500@gmail.com>
2020-04-14 12:04:44 +0200

    add component on page App.js for test, create a page TableScore.js and add a simple table score

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import TableScore from './components/TableScore'
 
 function App() {
   return (
     <div className="App">
-
+      <TableScore />
     </div>
   );
 }
---
+++ src/components/TableScore.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function TableScore() {
+  return (
+  <div className="TableScore">
+      <table>
+          <tr>
+              <td>#353</td>
+              <td>Team</td>
+              <td>score</td>
+          </tr>
+          <tr>
+              <td>#354</td>
+              <td>Team</td>
+              <td>score</td>
+          </tr>
+          <tr>
+              <td>#355</td>
+              <td>Team</td>
+              <td>score</td>
+          </tr>
+          <tr>
+              <td>#356</td>
+              <td>Team</td>
+              <td>score</td>
+          </tr>
+          <tr>
+              <td>#357</td>
+              <td>Team</td>
+              <td>score</td>
+          </tr>
+      </table>
+  </div>);
+}
+
+export default TableScore;
