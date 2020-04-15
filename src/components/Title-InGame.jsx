d1618b408e1352d5cd4e33318e32fdd494cdb9fb
Neo-Ryo <marco.sch4064@gmail.com>
2020-04-15 12:03:10 +0200

    added the score count part, and css to it

--- src/components/Title-InGame.jsx
+++ src/components/Title-InGame.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+// import VirusButton from "./components/VirusButton";
 import "semantic-ui-css/semantic.min.css";
 import "./Title-InGame.css";
 
-function TitleInGame() {
-  return <h1 className="title-in-game">Virus clicker</h1>;
-}
+class TitleInGame extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1 className="title-in-game">Virus clicker</h1>
+        <section className="score-count">
+          <p>score: {this.state.counter}</p>
 
+          <p>Help us!{/* this text will change during the game */}</p>
+        </section>
+      </div>
+    );
+  }
+}
 export default TitleInGame;
