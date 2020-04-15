209eec82d3d0ac16b7608b4bff5414b30315d57d
Marion-H <doudou6500@gmail.com>
2020-04-15 09:38:26 +0200

    switch class to className in page tablescore.js

--- src/components/TableScore.js
+++ src/components/TableScore.js
@@ -8,19 +8,19 @@ class TableScore extends React.Component {
     return (
       <div className="TableScore">
         <table className="ui celled collapsing very basic table">
-          <tbody class="">
+          <tbody className="">
             <tr>
               <td>#353</td>
-              <td class="">
-                <h4 class="ui image header">
+              <td className="">
+                <h4 className="ui image header">
                   <img
                     src="https://react.semantic-ui.com/images/avatar/small/lena.png"
-                    class="ui mini rounded image"
+                    className="ui mini rounded image"
                     alt=""
                   />
-                  <div class="content">
+                  <div className="content">
                     Lena
-                    <div class="sub header">Human Resources</div>
+                    <div className="sub header">Human Resources</div>
                   </div>
                 </h4>
               </td>
@@ -28,33 +28,33 @@ class TableScore extends React.Component {
             </tr>
             <tr>
               <td>#354</td>
-              <td class="">
-                <h4 class="ui image header">
+              <td className="">
+                <h4 className="ui image header">
                   <img
                     src="https://react.semantic-ui.com/images/avatar/small/matthew.png"
-                    class="ui mini rounded image"
+                    className="ui mini rounded image"
                     alt=""
                   />
-                  <div class="content">
+                  <div className="content">
                     Matthew
-                    <div class="sub header">Fabric Design</div>
+                    <div className="sub header">Fabric Design</div>
                   </div>
                 </h4>
               </td>
               <td>3000</td>
             </tr>
-            <tr>
+            <tr className="table-my-score-team">
               <td>#355</td>
-              <td class="">
-                <h4 class="ui image header">
+              <td className="">
+                <h4 className="ui image header">
                   <img
                     src="https://react.semantic-ui.com/images/avatar/small/lindsay.png"
-                    class="ui mini rounded image"
+                    className="ui mini rounded image"
                     alt=""
                   />
-                  <div class="content">
+                  <div className="content">
                     Lindsay
-                    <div class="sub header">Entertainment</div>
+                    <div className="sub header">Entertainment</div>
                   </div>
                 </h4>
               </td>
@@ -62,16 +62,16 @@ class TableScore extends React.Component {
             </tr>
             <tr>
               <td>#356</td>
-              <td class="">
-                <h4 class="ui image header">
+              <td className="">
+                <h4 className="ui image header">
                   <img
                     src="https://react.semantic-ui.com/images/avatar/small/mark.png"
-                    class="ui mini rounded image"
+                    className="ui mini rounded image"
                     alt=""
                   />
-                  <div class="content">
+                  <div className="content">
                     Mark
-                    <div class="sub header">Executive</div>
+                    <div className="sub header">Executive</div>
                   </div>
                 </h4>
               </td>
@@ -79,16 +79,16 @@ class TableScore extends React.Component {
             </tr>
             <tr>
               <td>#357</td>
-              <td class="">
-                <h4 class="ui image header">
+              <td className="">
+                <h4 className="ui image header">
                   <img
                     src="https://react.semantic-ui.com/images/avatar/small/mark.png"
-                    class="ui mini rounded image"
+                    className="ui mini rounded image"
                     alt=""
                   />
-                  <div class="content">
+                  <div className="content">
                     Mark
-                    <div class="sub header">Executive</div>
+                    <div className="sub header">Executive</div>
                   </div>
                 </h4>
               </td>
