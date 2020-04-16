0da6899dd9d51e08b53ccae37638f4999c2a1120
Charlyln <charleshenry.lenue@gmail.com>
2020-04-16 10:47:57 +0200

    change function name handleclick to increment in VirusButton file

--- src/components/VirusButton.jsx
+++ src/components/VirusButton.jsx
@@ -6,10 +6,10 @@ import "./VirusButton.css";
 class VirusButton extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+    this.increment = this.increment.bind(this);
     this.state = { counter: 0 };
   }
-  handleClick() {
+  increment() {
     this.setState({ counter: this.state.counter + 1 });
   }
 
@@ -19,7 +19,7 @@ class VirusButton extends React.Component {
         <Tada spy={this.state.counter}>
           <img
             className="logo"
-            onClick={this.handleClick}
+            onClick={this.increment}
             src={Virus}
             alt="logo"
           />
