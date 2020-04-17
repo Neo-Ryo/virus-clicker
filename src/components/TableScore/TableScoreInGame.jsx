5c77ecbbdb0eb1747ff31117fb38592b3a02b707
Marion-H <doudou6500@gmail.com>
2020-04-17 13:55:44 +0200

    change name file tablescore.css in tablescore.module.css and switch link in page tablescoreingame.jsx

--- src/components/TableScore/TableScoreInGame.jsx
+++ src/components/TableScore/TableScoreInGame.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Header, Image, Table,Button} from "semantic-ui-react";
-import "./style/tableScore.css";
+import styles from "./style/tableScore.module.css";
 
 class TableScoreInGame extends React.Component {
   constructor(props) {
