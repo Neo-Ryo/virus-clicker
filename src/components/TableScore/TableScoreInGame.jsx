e4097fd90b928b2efcf2c649f2a9b9bbbafaf99d
Marion-H <doudou6500@gmail.com>
2020-04-17 15:44:59 +0200

    size button css

--- src/components/TableScore/TableScoreInGame.jsx
+++ src/components/TableScore/TableScoreInGame.jsx
@@ -8,13 +8,13 @@ const team = [
     imageTeam: "https://react.semantic-ui.com/images/avatar/small/lena.png",
     pseudo: "Xena",
     score: "1 000 000",
-    rank: 3,
+    rank: 1,
   },
   {
     teamName: "YuGiHo",
     imageTeam: "https://react.semantic-ui.com/images/avatar/small/lena.png",
     pseudo: "Seito Kaiba",
-    score: "9 000 000",
+    score: "19 000 000",
     rank: 2,
   },
   {
@@ -22,35 +22,35 @@ const team = [
     imageTeam: "https://react.semantic-ui.com/images/avatar/small/lena.png",
     pseudo: "Neo Ryo",
     score: "7 944",
-    rank: 1,
+    rank: 3,
   },
 ];
 
 function TableScoreInGame() {
   return (
-    <Container className={styles.tableScore}>
-      <Button className={styles.buttonTable} color="teal" onClik={() => ""}>
-        +
-      </Button>
-      <Table basic="very" celled collapsing>
+    <Container>
+      <Button size="mini" color="teal" onClick={() => ""} content="+"></Button>
+      <Table className={styles.tableScore} basic="very" celled collapsing>
         <Table.Body>
-          {team.map((user) => (
-            <>
-              <Table.Row>
-                <Table.Cell>#{user.rank}</Table.Cell>
-                <Table.Cell>
-                  <Header as="h4" image>
-                    <Image src={user.imageTeam} rounded size="mini" />
-                    <Header.Content>
-                      {user.pseudo}
-                      <Header.Subheader>{user.teamName}</Header.Subheader>
-                    </Header.Content>
-                  </Header>
-                </Table.Cell>
-                <Table.Cell>{user.score}</Table.Cell>
-              </Table.Row>
-            </>
-          ))}
+          {team
+            .map((user) => (
+              <>
+                <Table.Row>
+                  <Table.Cell>#{user.rank}</Table.Cell>
+                  <Table.Cell>
+                    <Header as="h4" image>
+                      <Image src={user.imageTeam} rounded size="mini" />
+                      <Header.Content>
+                        {user.pseudo}
+                        <Header.Subheader>{user.teamName}</Header.Subheader>
+                      </Header.Content>
+                    </Header>
+                  </Table.Cell>
+                  <Table.Cell>{user.score}</Table.Cell>
+                </Table.Row>
+              </>
+            ))
+            .sort()}
         </Table.Body>
       </Table>
     </Container>
