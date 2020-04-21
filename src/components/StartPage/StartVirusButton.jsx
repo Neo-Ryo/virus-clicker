526bf47dcf46890a086601b713872c158a243ead
Charlyln <charleshenry.lenue@gmail.com>
2020-04-21 12:20:51 +0200

    add showButtons metod to show button when you click on the virus logo

--- src/components/StartPage/StartVirusButton.jsx
+++ src/components/StartPage/StartVirusButton.jsx
@@ -5,17 +5,29 @@ import { Image, Grid, Button } from "semantic-ui-react";
 import Zoom from "react-reveal/Zoom";
 import Fade from "react-reveal/Fade";
 
-
 class StartVirusButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: false };
+    this.showButtons = this.showButtons.bind(this);
+  }
+  showButtons() {
+    this.setState({ show: !this.state.show });
+  }
   render() {
     return (
       <Grid centered>
         <Grid.Column mobile={13} tablet={11} computer={7}>
           <Zoom left>
-            <Image className={`${styles.startButton}`} src={Virus} alt="logo" />
+            <Image
+              onClick={this.showButtons}
+              className={`${styles.startButton}`}
+              src={Virus}
+              alt="logo"
+            />
           </Zoom>
         </Grid.Column>
-        <Fade bottom>
+        <Fade bottom when={this.state.show}>
           <Button className="ui purple button">Create a team</Button>
           <Button className="ui purple button">Join a team</Button>
         </Fade>
