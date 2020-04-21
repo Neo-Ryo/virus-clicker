171bdcd67005e3b5811e6dec077ba0f85ba9fd17
Charlyln <charleshenry.lenue@gmail.com>
2020-04-21 12:03:46 +0200

    add start animation and remove margin to VirusLogo

--- src/components/StartPage/StartVirusButton.jsx
+++ src/components/StartPage/StartVirusButton.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Virus from "../virus.png";
 import styles from "./startVirusButton.module.css";
-import { Container, Image, Grid } from "semantic-ui-react";
+import { Image, Grid } from "semantic-ui-react";
+import Zoom from "react-reveal/Zoom";
 
 class StartVirusButton extends React.Component {
   render() {
     return (
       <Grid centered>
         <Grid.Column mobile={13} tablet={11} computer={7}>
+          <Zoom left>
             <Image className={`${styles.startButton}`} src={Virus} alt="logo" />
+          </Zoom>
         </Grid.Column>
       </Grid>
     );
