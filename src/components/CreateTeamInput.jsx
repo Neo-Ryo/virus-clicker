086ae9b29e93bfbe4e055d253a77af79a2c08c59
Christellec64 <christelle.cwcs64@gmail.com>
2020-04-22 10:08:55 +0200

    centered the component's items in CreateTeamInput.jsx

--- src/components/CreateTeamInput.jsx
+++ src/components/CreateTeamInput.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { Input, Icon } from "semantic-ui-react";
+import { Input, Icon, Grid, GridRow, GridColumn } from "semantic-ui-react";
 
 function CreateTeamInput(props) {
   return (
-    <>
-      <Input
-        label={{ icon: "asterisk" }}
-        labelPosition="right corner"
-        placeholder="Team name..."
-      />
-      <Icon name="checkmark" color="green" disabled />
-    </>
+    <Grid>
+      <GridRow columns={2} centered>
+        <GridColumn>
+          <Input
+            label={{ icon: "asterisk" }}
+            labelPosition="right corner"
+            placeholder="Team name..."
+          />
+          <Icon name="checkmark" color="green"/>
+        </GridColumn>
+      </GridRow>
+    </Grid>
   );
 }
 export default CreateTeamInput;
