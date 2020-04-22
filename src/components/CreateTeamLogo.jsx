08cd1878626ec9e2cce88ab5a554a13243a69731
Christellec64 <christelle.cwcs64@gmail.com>
2020-04-22 12:08:35 +0200

    Adding logo area and button to add a logo in the CreateTeamLogo component

--- src/components/CreateTeamLogo.jsx
+++ src/components/CreateTeamLogo.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import {
+  Placeholder,
+  Button,
+  Grid,
+  GridRow,
+  GridColumn,
+} from "semantic-ui-react";
 
-function CreateTeamLogo (props) {
-    return (
-        <>
-        </>
-    )
+function CreateTeamLogo(props) {
+  return (
+    <Grid>
+      <GridRow columns={2} centered>
+        <GridColumn>
+          <Placeholder style={{ height: 150, width: 150 }}>
+            <Placeholder.Image rounded />
+          </Placeholder>
+        </GridColumn>
+      </GridRow>
+      <GridRow columns={3} centered>
+        <GridColumn>
+          <Button color="teal" icon="add" content="Add a logo" size="tiny"/>
+        </GridColumn>
+      </GridRow>
+    </Grid>
+  );
 }
 
 export default CreateTeamLogo;
