e4af3bb1ddc1e832cd5340d4cbc92f53dfff7d56
Charlyln <charleshenry.lenue@gmail.com>
2020-04-23 15:36:28 +0200

    add component StartVirus to the route in router

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import StartVirusButton from "./StartPage/StartVirusButton"
 
 
 function Router() {
   return (
     <BrowserRouter>
         <Switch>
-          <Route exact path="/"  />
+          <Route exact path="/" component={StartVirusButton}/>
           <Route path="/createteam" />
           <Route path="/jointeam"  />
           <Route path="/game"  />
