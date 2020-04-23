c42cd9b699ca8e7aa8d15b9173571892b3c93459
Marion-H <doudou6500@gmail.com>
2020-04-23 11:05:33 +0200

    create a handleSubmit to detect if this pseudo is taken or not

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -1,43 +1,46 @@
 import React from "react";
 import { Input } from "semantic-ui-react";
-import axios from 'axios'
-
+import axios from "axios";
 
 class CreateUser extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       pseudoTape: "",
-      users:{}
+      users: {},
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(event) {
-      this.setState({ pseudoTape: event.target.value });
-    }
-
-    getUsers() {
-      //const infoPokemon = result.data;
-      axios
-        .get(
-          `https://virusclicker.herokuapp.com/users`
-        )
-        .then(res => {
-          this.setState({ users: res.data[0]});
-          console.log(res.data[0])
-        });
-    }
-
-  componentDidMount() {
-   this.getUsers()
-   
+    this.setState({ pseudoTape: event.target.value }); 
   }
 
+  getUsers() {
+    axios.get(`https://virusclicker.herokuapp.com/users`).then((res) => {
+      this.setState({ users: res.data });
+      
+    });
+  }
+  handleSubmit(){
+    const pseudos = this.state.users.map(user => user.pseudo)
+    if (pseudos.find(pseudo => pseudo.toLowerCase() === this.state.pseudoTape.toLocaleLowerCase())){
+      alert("This pseudo is already taken.")
+    }else{
+      console.log('ok')
+      {/*await POST method to API */}
+    }
+  }
 
+  componentDidMount() {
+    this.getUsers();
+  }
+  
 
   render() {
     return (
       <>
+      
         <Input
           placeholder="Pseudo"
           label={{ color: "red", corner: "right", icon: "asterisk" }}
@@ -46,8 +49,7 @@ class CreateUser extends React.Component {
           onChange={this.handleChange}
         />
         <p>{this.state.pseudoTape}</p>
-        
-        
+        <button onClick={this.handleSubmit}>Submit</button>
       </>
     );
   }
