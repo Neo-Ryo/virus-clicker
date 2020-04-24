45df77e56cea767c4b5be24aa9c9f16233fc578e
Christellec64 <christelle.cwcs64@gmail.com>
2020-04-24 09:16:29 +0200

    removed button + in CreateTeamLogo.jsx

--- src/components/CreateTeamLogo.jsx
+++ src/components/CreateTeamLogo.jsx
@@ -12,16 +12,13 @@ function CreateTeamLogo({logo, getUrl}) {
   return (<>
     <Grid>
       <GridRow columns={2}>
-        <Input type="url" label="Url" placeholder="http://virus_clicker.com" value={logo} onChange={getUrl}/>
+        <Input type="url" label="Url image" placeholder="http://virus_clicker.com" value={logo} onChange={getUrl}/>
       </GridRow>
       <GridRow columns={3}>
         <GridColumn verticalAlign="middle">
           <Placeholder style={{ height: 150, width: 150 }}>
             <Placeholder.Image/>
           </Placeholder>
-        </GridColumn>
-        <GridColumn verticalAlign="bottom">
-          <Button size="tiny">+</Button>
         </GridColumn>
       </GridRow>
     </Grid>
