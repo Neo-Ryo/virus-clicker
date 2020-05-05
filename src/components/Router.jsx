3bd000d6357113822c96fc0e065c8effbeacf57b
Neo-Ryo <marco.sch4064@gmail.com>
2020-05-05 16:46:10 +0200

    changed .js to jsx

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Register from "./Register";
-import GamePage from "./gamepage/GamePage";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Register from './Register';
+import GamePage from './gamepage/GamePage';
 
 function Router() {
   return (
