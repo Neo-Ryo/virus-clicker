63045a0c588ddf7be601407e89c869c354e58d6a
Christellec64 <christelle.cwcs64@gmail.com>
2020-05-11 12:02:04 +0200

    changing image tag in planet component

--- src/components/gamepage/Planet.jsx
+++ src/components/gamepage/Planet.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Progress, Statistic } from 'semantic-ui-react';
+import { Container, Progress, Statistic, Image } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import PLanetLogo from './images/earth.png';
 import styles from './styles/planet.module.css';
@@ -17,7 +17,7 @@ export default function Planet({ percentage }) {
   }
   return (
     <Container textAlign="center">
-      <img className={styles.logoplanet} src={PLanetLogo} alt="Planet logo" />
+      <Image className={styles.logoplanet} src={PLanetLogo} alt="Planet logo" />
       <Statistic size="tiny" color={color}>
         <Statistic.Value>{`${percentage} %`}</Statistic.Value>
       </Statistic>
