ac2a3ebdd321689b93cf58b9a5cf76301c230f4b
Christellec64 <christelle.cwcs64@gmail.com>
2020-05-11 11:57:03 +0200

    adding colors for percentage in planet component

--- src/components/gamepage/Planet.jsx
+++ src/components/gamepage/Planet.jsx
@@ -5,13 +5,23 @@ import PLanetLogo from './images/earth.png';
 import styles from './styles/planet.module.css';
 
 export default function Planet({ percentage }) {
+  let color = null;
+  if (percentage <= 30) {
+    color = 'red';
+  } else if (percentage > 30 && percentage < 50) {
+    color = 'orange';
+  } else if (percentage >= 50 && percentage < 70) {
+    color = 'yellow';
+  } else {
+    color = 'green';
+  }
   return (
     <Container textAlign="center">
       <img className={styles.logoplanet} src={PLanetLogo} alt="Planet logo" />
-      <Statistic size="tiny">
+      <Statistic size="tiny" color={color}>
         <Statistic.Value>{`${percentage} %`}</Statistic.Value>
       </Statistic>
-      <Progress progress percent={percentage} indicating />
+      <Progress percent={percentage} indicating />
     </Container>
   );
 }
