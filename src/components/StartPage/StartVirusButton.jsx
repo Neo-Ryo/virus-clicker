0c2af0c604adcdf00b6111c0686e7bd278ba44ed
Charlyln <charleshenry.lenue@gmail.com>
2020-05-23 01:06:28 +0200

    change to reactstrap, change background

--- src/components/StartPage/StartVirusButton.jsx
+++ src/components/StartPage/StartVirusButton.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Header, Image, Grid, Button } from 'semantic-ui-react';
 import Zoom from 'react-reveal/Zoom';
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-router-dom';
 import styles from './startVirusButton.module.css';
 import Virus from '../gamepage/images/virus.png';
+import { Button, Container, Row, Col } from 'reactstrap';
 
 class StartVirusButton extends React.Component {
   constructor(props) {
@@ -29,41 +29,65 @@ class StartVirusButton extends React.Component {
   render() {
     const { show, uuid } = this.state;
     return (
-      <div
-        style={{ minHeight: '102vh', margin: '0px !important' }}
-        className={styles.backgrd}
-      >
+      <div>
         <Zoom left>
-          <Grid centered>
-            <Grid.Row style={{ height: '80vw' }}>
-              <Header className={styles.title} as="h1">
-                Virus Clicker
-              </Header>
-            </Grid.Row>
-            <Grid.Row>
-              <Image
-                onClick={this.showButtons}
-                className={`${styles.startButton} ${
-                  show ? styles.virusButtonActive : styles.virusButtonNotActive
-                }`}
-                src={Virus}
-                alt="logo"
-              />
-            </Grid.Row>
-            <Grid.Row>
-              <Fade bottom when={show}>
-                {uuid ? (
-                  <Link to={`/game/${uuid}`}>
-                    <Button color="teal">Go!</Button>
-                  </Link>
-                ) : (
-                  <Link to="/register">
-                    <Button className="ui purple button">Register</Button>
-                  </Link>
-                )}
-              </Fade>
-            </Grid.Row>
-          </Grid>
+          <Container>
+            <Row>
+              <Col
+                style={{ textAlign: 'center' }}
+                xs={{ size: 12, offset: 0 }}
+                sm={{ size: 12, offset: 0 }}
+                md={{ size: 4, offset: 4 }}
+                lg={{ size: 6, offset: 3 }}
+              >
+                <h1
+                  style={{
+                    marginTop: '50px',
+                    marginBottom: '50px',
+                  }}
+                >
+                  Virus Clicker
+                </h1>
+                <img
+                  onClick={this.showButtons}
+                  className={`${styles.startButton} ${
+                    show
+                      ? styles.virusButtonActive
+                      : styles.virusButtonNotActive
+                  }`}
+                  src={Virus}
+                  alt="logo"
+                />
+                <Fade bottom when={show}>
+                  {uuid ? (
+                    <Link to={`/game/${uuid}`}>
+                      <Button
+                        color="danger"
+                        size="lg"
+                        style={{
+                          marginTop: '30px',
+                        }}
+                      >
+                        Go!
+                      </Button>
+                    </Link>
+                  ) : (
+                    <Link to="/register">
+                      <Button
+                        color="danger"
+                        size="lg"
+                        style={{
+                          marginTop: '30px',
+                        }}
+                      >
+                        Register
+                      </Button>
+                    </Link>
+                  )}
+                </Fade>
+              </Col>
+            </Row>
+          </Container>
         </Zoom>
       </div>
     );
